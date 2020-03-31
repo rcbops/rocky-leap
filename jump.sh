264c5f7a5f360a52d918c245640421f0201725d9
Jake Briggs <briggs679@gmail.com>
2020-03-31 09:36:06 -0500

    Added cleanup for services that are combined into single containers

--- jump.sh
+++ jump.sh
@@ -50,15 +50,19 @@ function run_lock {
 
 function upgrade_database {
   pushd /root/upgrades
-    RUN_TASKS=("/root/upgrades/prep-upgrade.yml")
+    RUN_TASKS=("/root/upgrades/pre-upgrade-backup.yml")
+    RUN_TASKS+=("/root/upgrades/prep-upgrade.yml")
     RUN_TASKS+=("/root/upgrades/build-configs.yml")
     RUN_TASKS+=("/root/upgrades/build-venvs.yml")
-    RUN_TASKS+=("/root/upgrades/pre-upgrade-backup.yml")
+    RUN_TASKS+=("/root/upgrades/shutdown-containers.yml")
     RUN_TASKS+=("/root/upgrades/upgrade-database-ocata.yml")
     RUN_TASKS+=("/root/upgrades/upgrade-database-pike.yml")
     RUN_TASKS+=("/root/upgrades/upgrade-database-queens.yml")
     RUN_TASKS+=("/root/upgrades/upgrade-database-rocky.yml")
     RUN_TASKS+=("/root/upgrades/post-upgrade-backup.yml")
+    RUN_TASKS+=("/root/upgrades/cheanup-heat.yml")
+    RUN_TASKS+=("/root/upgrades/cheanup-ironic.yml")
+    RUN_TASKS+=("/root/upgrades/cheanup-nova.yml")
     for item in ${!RUN_TASKS[@]}; do
       run_lock $item "${RUN_TASKS[$item]}"
     done
@@ -95,25 +99,8 @@ function pre_flight {
 function main {
     pre_flight
     pushd /opt/openstack-ansible
-        RUN_TASKS=("/opt/openstack-ansible/playbooks/lxc-containers-destroy.yml -e force_containers_destroy=true -e force_containers_data_destroy=true")
-        # DELETE LINES UNTIL MARKER TO CONTINUE
-        echo "*****************************"
-        echo " *** Manual intervention required at this point (automation not built yet) 888"
-        echo ""
-        echo "Please remove all of the following containers from ansible-inventory"
-        echo ""
-        echo "- nova_api_metadata_container-*"
-        echo "- nova_api_os_compute_container-*"
-        echo "- nova_cert_container-*"
-        echo "- nova_conductor_container-*"
-        echo "- nova_console_container-*"
-        echo "- nova_scheduler_container-*"
-        echo ""
         cp /opt/openstack-ansible/inventory/env.d/nova.yml /etc/openstack_deploy/env.d
-        echo "/etc/openstack_deploy/env.d/nova.ymnl has been updated for new skel.  Update any other files needed at this time."
-        echo "Delete lines 99-116 and then re-run jump.sh to continue installation"
-        exit 0
-        # MARKER! STOP DELETING LINES
+        RUN_TASKS=("/opt/openstack-ansible/playbooks/lxc-containers-destroy.yml -e force_containers_destroy=true -e force_containers_data_destroy=true")
         RUN_TASKS+=("/opt/openstack-ansible/playbooks/setup-hosts.yml -f 50")
         RUN_TASKS+=("/root/upgrades/venv_install.yml")
         RUN_TASKS+=("/opt/openstack-ansible/playbooks/setup-infrastructure.yml -f 50")
